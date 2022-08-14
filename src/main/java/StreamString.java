e9352d0de5e0df08b5708548d4c984245cf6469b
Oksana <briliant621621@gmail.com>
2022-08-14 15:27:47 +0300

    Changes StreamString.java

--- src/main/java/StreamString.java
+++ src/main/java/StreamString.java
@@ -1,9 +1,11 @@
 package main.java;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 
 public class StreamString {
@@ -27,7 +29,28 @@ public class StreamString {
         Arrays.stream(String.join(",", array).replace(", ", ",").split(","))
                 .mapToInt(Integer::parseInt)
                 .sorted()
-                .mapToObj(String::valueOf)
                 .forEach(s -> System.out.print(s + ","));
+    }
+
+    int seed() {
+
+        return new Random().nextInt();
+
+    }
+
+    public void streamIterator(long a, int c, int m, long[] x) {
+        x[0] = seed();
+       /*
+        Это я с цыклом проверяла. Потом удалю
+        for (int i = 1; i < 10; i++) {
+            x[i] = (a * x[i - 1] + c) % m;
+            System.out.print(x[i] + "  ");
+     } */
+            System.out.println();
+        Stream
+                .iterate(1, i -> i < 10, i -> i + 1)
+                .mapToLong(i -> (a * x[i - 1] + c) % m)
+                .forEach(s -> System.out.print(s + "  "));
+
     }
 }
--- src/main/java/StreamStrings.java
+++ src/main/java/StreamStrings.java
@@ -5,10 +5,12 @@ public class StreamStrings {
     public static void main(String[] args) {
         StreamString streamString = new StreamString();
         String[] list = {"One", "Two", "Three", "Four", "Five", "Six"};
-        String[] list1 = {"1, 12, 20", "44, 5"};
+        String[] list1 = {"1, 12, 20", "44, 5"};;
         streamString.metod1(list);
         streamString.metod2(list);
         System.out.println();
         streamString.metodStream(list1);
+        System.out.println();
+        streamString.streamIterator(25214903917L,11,(int)Math.pow(2,48),new long[10]);
     }
 }
